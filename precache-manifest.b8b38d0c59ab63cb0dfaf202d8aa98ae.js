d038d46c40cbc65c60608fd4ac7027e152cdd2bc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-20 16:19:49 +0000

    Deploy lihqi/test-travis-ci to github.com/lihqi/test-travis-ci.git:gh-pages

---
+++ precache-manifest.b8b38d0c59ab63cb0dfaf202d8aa98ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aee0fb32be832b7f6d27089d7878c53",
+    "url": "/test-travis-ci/index.html"
+  },
+  {
+    "revision": "16e492846f183db2fb7e",
+    "url": "/test-travis-ci/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "742aa8f1156a4efb5ad2",
+    "url": "/test-travis-ci/static/js/2.9345808c.chunk.js"
+  },
+  {
+    "revision": "16e492846f183db2fb7e",
+    "url": "/test-travis-ci/static/js/main.9c1c4c2c.chunk.js"
+  },
+  {
+    "revision": "fd00a49cb6a31403884e",
+    "url": "/test-travis-ci/static/js/runtime-main.e78a2ed5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test-travis-ci/static/media/logo.25bf045c.svg"
+  }
+]);
